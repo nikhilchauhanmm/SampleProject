977042a0607861e2534381a37b7e5abd19af3008
Nikhil Chauhan <nikhil.chauhan@mutualmobile.com>
2021-10-26 16:41:47 +0530

    2nd commit

--- app/src/main/java/com/example/sampleproject/MainActivity.kt
+++ app/src/main/java/com/example/sampleproject/MainActivity.kt
@@ -14,12 +14,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SampleProjectTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+            // A surface container using the 'background' color from the theme
+            Surface(color = MaterialTheme.colors.background) {
+                Greeting("Android")
             }
+
         }
     }
 }
